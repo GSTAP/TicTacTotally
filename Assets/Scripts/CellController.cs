edf8d92fc41c77de8e4c5f2654bc919406873f95
Tim <bullzaye007@gmail.com>
2016-03-26 18:32:06 -0500

    Initial Commit

---
+++ Assets/Scripts/CellController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellController : MonoBehaviour {
+
+	private SpriteRenderer spRender;
+
+	public Sprite xPiece;
+	public Sprite yPiece;
+
+	void Awake()
+	{
+		spRender = GetComponent<SpriteRenderer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		if (spRender.sprite == null)
+		{
+			spRender.sprite = xPiece;
+		}
+	}
+}
